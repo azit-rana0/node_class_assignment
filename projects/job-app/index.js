31ecc8b78a13aa2b307519e9c77aa0426405b835
azit-rana0 <ajeetrana520@gmail.com>
2025-07-01 10:56:43 +0530

    added more file .env

--- projects/job-app/index.js
+++ projects/job-app/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const jobRoutes = require("./route/job.route");
 
 const app = express();
+dotenv.config();
 
 // Middleware
 app.use(express.json());
@@ -11,17 +13,14 @@ app.use(express.json());
 // Connection with mongoDB
 // mongodb://server:port/db_name
 mongoose
-  // .connect("mongodb://localhost:27017/jobapp")
-  .connect(
-    "mongodb+srv://azrrana1236:NVFB4bBj4EHPmmrt@cluster0.dwajzim.mongodb.net/"
-  )
+  .connect(process.env.DB_URL)
   .then(() => console.log("DB connecting successfully"))
   .catch((err) => console.log("Error connecting database", err));
 
 // Routes
 app.use("/api/v1/job", jobRoutes);
 
-const portNo = 8080;
+const portNo = process.env.PORT;
 
 app.listen(8080, () => {
   console.log(`Server is up and running at port ${portNo}`);
